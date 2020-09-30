953cdd1bfd99f5629b37dc453e18cf8036f03d2a
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-09-30 23:57:20 +0900

    changed handler

--- mojacoder-backend/lib/mojacoder-backend-stack.ts
+++ mojacoder-backend/lib/mojacoder-backend-stack.ts
@@ -27,7 +27,7 @@ export class MojacoderBackendStack extends cdk.Stack {
         pool.addClient("mojacoder-frontend-app");
         const signupTrigger = new Function(this, 'signup-trigger', {
             runtime: Runtime.GO_1_X,
-            handler: 'pre-signup',
+            handler: 'handler',
             code: Code.fromAsset(join(__dirname, '../cognito-triggers/pre-signup')),
         });
         const JudgeQueue = new Queue(this, 'JudgeQueue');
