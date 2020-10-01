5018d96b66a4d663eb3222df95be9f21883872fc
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-10-01 23:04:06 +0900

    + 404.tsx

--- mojacoder-frontend/pages/404.tsx
+++ mojacoder-frontend/pages/404.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Alert } from 'react-bootstrap'
 
 const Error: React.FC = () => {
-    return (
-        <Alert variant="danger">存在しないページか、権限がありません。</Alert>
-    )
+    return <Alert variant="danger">存在しないページです。</Alert>
 }
 export default Error
