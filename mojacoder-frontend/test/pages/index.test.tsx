bd1038f69c2565da92668c5e2f6f538733c13fd6
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-10-05 01:49:18 +0900

    fixed test

--- mojacoder-frontend/test/pages/index.test.tsx
+++ mojacoder-frontend/test/pages/index.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import { Index } from '../../pages/index'
 
 describe('Home page', () => {
     it('matches snapshot', () => {
-        const { asFragment } = render(<Home />, {})
+        const { asFragment } = render(<Index />, {})
         expect(asFragment()).toMatchSnapshot()
     })
 
     it('clicking button triggers alert', () => {
-        const { getByText } = render(<Home />, {})
+        const { getByText } = render(<Index />, {})
         window.alert = jest.fn()
         fireEvent.click(getByText('Test Button'))
         expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
