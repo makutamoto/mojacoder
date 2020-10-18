b69c95e45b19701c8085f06a51fbfa2f0a9d4632
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-10-19 02:08:54 +0900

    - l

--- mojacoder-frontend/next.config.js
+++ mojacoder-frontend/next.config.js
@@ -21,7 +21,7 @@ module.exports = (phase) => {
                     USER_POOL_ID: 'ap-northeast-1_v9BRY34ef',
                     USER_POOL_CLIENT_ID: '2lcpbuka7ago7r8gk2a9d58f8s',
                     APPSYNC_ENDPOINT:
-                        'https://toheblmzlnfbfckl74t2nnndri.appsync-api.ap-northeast-1.amazonaws.com/graphqll',
+                        'https://toheblmzlnfbfckl74t2nnndri.appsync-api.ap-northeast-1.amazonaws.com/graphql',
                     APPSYNC_APIKEY: 'da2-zf3whlcplbhorhietu4h5bfk5y',
                     COOKIE_DOMAIN: 'mojacoder.vercel.app',
                 },
