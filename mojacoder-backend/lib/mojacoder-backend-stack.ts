dbf0e911c63c5e7ccb49c816d79d37b57a32bcf1
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-10-29 01:36:18 +0900

    + permission to submit resolver

--- mojacoder-backend/lib/mojacoder-backend-stack.ts
+++ mojacoder-backend/lib/mojacoder-backend-stack.ts
@@ -153,8 +153,8 @@ export class MojacoderBackendStack extends cdk.Stack {
             },
         });
         submitCodeResolverLambda.addToRolePolicy(new PolicyStatement({
-            resources: [submissionTable.tableArn, submittedCodeBucket.bucketArn],
-            actions: ['dynamodb:PutItem', 's3:PutObject'],
+            resources: [submissionTable.tableArn, submittedCodeBucket.bucketArn, JudgeQueue.queueArn],
+            actions: ['dynamodb:PutItem', 's3:PutObject', 'sqs:SendMessage'],
         }));
         const submitCodeResolverLambdaDatasource = api.addLambdaDataSource('submitCodeResolverLambda', submitCodeResolverLambda);
         submitCodeResolverLambdaDatasource.createResolver({
