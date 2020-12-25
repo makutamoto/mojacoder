e49914947b9f26dfd1dbb47c4bf2c10649e9a635
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-12-25 12:14:06 +0900

    + ISR for testcases tab

--- mojacoder-frontend/pages/users/[username]/problems/[problemID]/testcases/index.tsx
+++ mojacoder-frontend/pages/users/[username]/problems/[problemID]/testcases/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { GetServerSideProps } from 'next'
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { Spinner, Table } from 'react-bootstrap'
@@ -11,40 +11,17 @@ import Layout from '../../../../../../components/Layout'
 import ProblemTop from '../../../../../../containers/ProblemTop'
 import { join } from 'path'
 
-const GetTestcaseNames = gql`
-    query GetTestcaseNames($authorUsername: String!, $problemID: ID!) {
-        user(username: $authorUsername) {
-            problem(id: $problemID) {
-                testcaseNames
-            }
-        }
-    }
-`
-interface GetTestcaseNamesResponse {
-    user: UserDetail | null
-}
-
 interface Props {
-    problem: Problem
+    problem?: Problem
 }
 const Submissions: React.FC<Props> = (props) => {
     const { query, pathname } = useRouter()
     const { problem } = props
-    const [testcaseNames, setTestcaseNames] = useState<string[] | null>(null)
-    useEffect(() => {
-        invokeQueryWithApiKey(GetTestcaseNames, {
-            authorUsername: query.username,
-            problemID: query.problemID,
-        }).then((data: GetTestcaseNamesResponse) => {
-            const { testcaseNames } = data.user.problem
-            setTestcaseNames(testcaseNames)
-        })
-    }, [query])
     return (
         <>
             <ProblemTop activeKey="testcases" problem={problem} />
             <Layout>
-                {testcaseNames === null ? (
+                {problem?.testcaseNames === null ? (
                     <div className="text-center">
                         <Spinner animation="border" />
                     </div>
@@ -56,7 +33,7 @@ const Submissions: React.FC<Props> = (props) => {
                             </tr>
                         </thead>
                         <tbody>
-                            {testcaseNames.map((name) => (
+                            {problem?.testcaseNames.map((name) => (
                                 <tr key={name}>
                                     <td>
                                         <Link
@@ -85,33 +62,43 @@ const Submissions: React.FC<Props> = (props) => {
 }
 export default Submissions
 
-const GetProblemOverview = gql`
-    query GetProblemOverview($username: String!, $id: ID!) {
+const GetTestcaseNames = gql`
+    query GetTestcaseNames($username: String!, $problemID: ID!) {
         user(username: $username) {
-            problem(id: $id) {
+            problem(id: $problemID) {
                 title
                 user {
                     detail {
                         screenName
                     }
                 }
+                testcaseNames
             }
         }
     }
 `
-interface GetProblemOverviewResponse {
+interface GetTestcaseNamesResponse {
     user: UserDetail | null
 }
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-    query,
-}) => {
-    const res = (await invokeQueryWithApiKey(GetProblemOverview, {
-        username: query.username,
-        id: query.problemID,
-    })) as GetProblemOverviewResponse
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+    const res = (await invokeQueryWithApiKey(GetTestcaseNames, {
+        username: params.username || '',
+        problemID: params.problemID || '',
+    })) as GetTestcaseNamesResponse
+    if (res.user === null || res.user.problem === null) {
+        return {
+            notFound: true,
+        }
+    }
     return {
         props: {
             problem: res.user.problem,
         },
+        revalidate: 1,
     }
 }
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+    paths: [],
+    fallback: true,
+})
