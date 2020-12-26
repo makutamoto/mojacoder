10392e23cc06eff1045664dd7aa103b56517005d
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-12-26 16:36:45 +0900

    new api key

--- mojacoder-frontend/next.config.js
+++ mojacoder-frontend/next.config.js
@@ -14,8 +14,8 @@ module.exports = (phase) => {
                     USER_POOL_ID: 'ap-northeast-1_rPJ5fZCy0',
                     USER_POOL_CLIENT_ID: '1iebksqvkjmo3jbmnq7h16688c',
                     APPSYNC_ENDPOINT:
-                        'https://6ts42hdrufflnirxr4nqsclaa4.appsync-api.ap-northeast-1.amazonaws.com/graphql',
-                    APPSYNC_APIKEY: 'da2-7rkmupgngreipjuqs3uqobxksq',
+                        'https://zbw5rgbp6nbplmvbddkkwm7m3a.appsync-api.ap-northeast-1.amazonaws.com/graphql',
+                    APPSYNC_APIKEY: 'da2-eyze5l444jd45o2oooqjcjrkaa',
                     COOKIE_DOMAIN: 'localhost',
                     ORIGIN: 'http://localhost:3000',
                 },
@@ -28,8 +28,8 @@ module.exports = (phase) => {
                     USER_POOL_ID: 'ap-northeast-1_rPJ5fZCy0',
                     USER_POOL_CLIENT_ID: '1iebksqvkjmo3jbmnq7h16688c',
                     APPSYNC_ENDPOINT:
-                        'https://6ts42hdrufflnirxr4nqsclaa4.appsync-api.ap-northeast-1.amazonaws.com/graphql',
-                    APPSYNC_APIKEY: 'da2-7rkmupgngreipjuqs3uqobxksq',
+                        'https://zbw5rgbp6nbplmvbddkkwm7m3a.appsync-api.ap-northeast-1.amazonaws.com/graphql',
+                    APPSYNC_APIKEY: 'da2-eyze5l444jd45o2oooqjcjrkaa',
                     COOKIE_DOMAIN: 'mojacoder.vercel.app',
                     ORIGIN: 'https://mojacoder.vercel.app',
                 },
