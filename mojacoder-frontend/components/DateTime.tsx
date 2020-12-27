b6abf3d02799d55bf9b240d0e063759ba12515bd
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-12-27 19:53:11 +0900

    + <time> to DateTime

--- mojacoder-frontend/components/DateTime.tsx
+++ mojacoder-frontend/components/DateTime.tsx
@@ -9,13 +9,14 @@ const DateTime: React.FC<DateTimeProps> = (props) => {
         return new Date(props.children || null)
     }, [props.children])
     return (
-        <>{`${datetime.getFullYear()}-${('0' + (datetime.getMonth() + 1)).slice(
-            -2
-        )}-${('0' + datetime.getDate()).slice(-2)} ${(
+        <time dateTime={datetime.toISOString()}>{`${datetime.getFullYear()}-${(
+            '0' +
+            (datetime.getMonth() + 1)
+        ).slice(-2)}-${('0' + datetime.getDate()).slice(-2)} ${(
             '0' + datetime.getHours()
         ).slice(-2)}:${('0' + datetime.getMinutes()).slice(-2)}:${(
             '0' + datetime.getSeconds()
-        ).slice(-2)}`}</>
+        ).slice(-2)}`}</time>
     )
 }
 export default DateTime
