905834f55cfb2fc8de107efcd81cdcf591d83ff5
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-12-28 21:26:22 +0900

    + responsive margin

--- mojacoder-frontend/components/Layout.tsx
+++ mojacoder-frontend/components/Layout.tsx
@@ -3,9 +3,12 @@ import { Container } from 'react-bootstrap'
 
 const Layout: React.FC = (props) => {
     return (
-        <Container className="mb-4 p-4 bg-white shadow rounded">
-            {props.children}
-        </Container>
+        <>
+            <Container className="p-4 bg-white shadow rounded">
+                {props.children}
+            </Container>
+            <div className="mt-4 d-sm-block d-none" />
+        </>
     )
 }
 export default Layout
