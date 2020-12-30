af1a8fb3a8f90acd370e028b634bb7167842f123
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-12-30 16:56:01 +0900

    + debug

--- mojacoder-frontend/containers/ProblemTop.tsx
+++ mojacoder-frontend/containers/ProblemTop.tsx
@@ -167,7 +167,7 @@ const ProblemTop: React.FC<ProblemTopProps> = (props) => {
                                 )
                             )}`}
                         >
-                            Tweet
+                            Tweet ({defaultLocale})
                         </a>
                     </div>
                 </div>
