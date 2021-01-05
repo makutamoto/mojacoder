8f01147868604a910172e6f8340191a2c8299bd4
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2021-01-05 19:18:13 +0900

    fixed Username

--- mojacoder-frontend/components/Username.tsx
+++ mojacoder-frontend/components/Username.tsx
@@ -4,18 +4,22 @@ import Link from 'next/link'
 import { UserDetail } from '../lib/backend_types'
 import UserIcon from './UserIcon'
 
+import styles from '../css/align.module.css'
+
 export interface UsernameProps {
     children?: UserDetail
 }
 
 const Username: React.FC<UsernameProps> = (props) => {
     return (
-        <>
+        <span>
             <UserIcon size={24}>{props.children}</UserIcon>{' '}
             <Link href={`/users/${props.children?.screenName}`} passHref>
-                <a className="align-middle">{props.children?.screenName}</a>
+                <a className={styles['align-super']}>
+                    {props.children?.screenName}
+                </a>
             </Link>
-        </>
+        </span>
     )
 }
 export default Username
