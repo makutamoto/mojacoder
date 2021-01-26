c7d2bdaa8aa35ad193eb474a404f23676ca994c4
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2021-01-27 00:05:48 +0900

    + Syntax highlight and markdown to Sample Component

--- mojacoder-frontend/components/Markdown.tsx
+++ mojacoder-frontend/components/Markdown.tsx
@@ -2,23 +2,40 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import Tex from '@matejmazur/react-katex'
 import math from 'remark-math'
+import clsx from 'clsx'
 
 import Sample from './Sample'
+import { LANGUAGE_TO_MODE } from './Editor'
+
+import styles from './Markdown.module.css'
 
 export interface MarkdownProps {
     source: string
+    preventLastMargin?: boolean
 }
 
-const Markdown: React.FC<MarkdownProps> = ({ source }) => {
+const Markdown: React.FC<MarkdownProps> = ({ source, preventLastMargin }) => {
     return (
         <ReactMarkdown
-            className="text-break"
+            className={clsx(
+                'text-break',
+                preventLastMargin && styles.paragraph
+            )}
             source={source}
             plugins={[math]}
             renderers={{
-                code: ({ language, value }) => (
-                    <Sample title={language} value={value} />
-                ),
+                code: ({ language, value }) => {
+                    const [langTemp, ...titleTemp] = language?.split(':') || [
+                        '',
+                    ]
+                    let lang = langTemp
+                    let title = titleTemp.join(':')
+                    if (lang !== 'md' && LANGUAGE_TO_MODE[lang] === undefined) {
+                        lang = ''
+                        title = language
+                    }
+                    return <Sample lang={lang} title={title} value={value} />
+                },
                 heading: (props) => {
                     const H = `h${Math.min(
                         6,
