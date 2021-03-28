9946efbf616a495a2152173c6c23f29bdfa461f9
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2021-03-29 00:28:10 +0900

    updated top page

--- mojacoder-frontend/pages/index.tsx
+++ mojacoder-frontend/pages/index.tsx
@@ -1,39 +1,77 @@
 import React from 'react'
-import Image from 'next/image'
-import { MarkGithubIcon, SmileyIcon } from '@primer/octicons-react'
+import { MarkGithubIcon } from '@primer/octicons-react'
+import { Container, Image } from 'react-bootstrap'
 
-import { useI18n } from '../lib/i18n'
 import IconWithText from '../components/IconWithText'
 import Layout from '../components/Layout'
-import Top from '../components/Top'
 import Title from '../components/Title'
 
 const GITHUB_LINK = 'https://github.com/makutamoto/mojacoder'
 const TWITTER_LINK = 'https://twitter.com/makutamoto'
 
 export const Index: React.FC = () => {
-    const { t } = useI18n('home')
     return (
         <>
             <Title />
-            <Top>
-                <div className="text-center">
-                    <Image
-                        width={512}
-                        height={256}
-                        src="/illustrations/undraw_programming_2svr.svg"
-                    />
-                    <h2 className="mt-4">{t`description`}</h2>
+            <div
+                className="d-flex flex-column justify-content-center bg-dark"
+                style={{ height: 'calc(100vh - 56px)' }}
+            >
+                <Container style={{ marginTop: '-1.5rem' }}>
+                    <h1>
+                        <Image src="/images/logo.svg" width={64} />{' '}
+                        <span className="align-middle text-white">
+                            MojaCoder
+                        </span>
+                    </h1>
+                    <h2>
+                        <span
+                            style={{
+                                background:
+                                    'linear-gradient(to right, #DC3545, #007BFF)',
+                                WebkitBackgroundClip: 'text',
+                                color: 'transparent',
+                            }}
+                        >
+                            <span className="d-inline-block">解けた！を</span>
+                            <span className="d-inline-block">
+                                私も作りたい。
+                            </span>
+                        </span>
+                    </h2>
+                </Container>
+            </div>
+            <div style={{ marginTop: '-1.5rem' }}>
+                <Layout>
+                    <h3>MojaCoderとは？</h3>
+                    <p>
+                        MojaCoderは競技プログラミングの新たな作問プラットフォームです。
+                        <br />
+                        初心者から上級者まで気軽に問題を作成して、共有できる環境を目指しています。
+                    </p>
+                </Layout>
+                <Layout>
+                    <h4>投稿に必要なのはユーザー登録のみ</h4>
+                    <p>
+                        MojaCoderでの作問にレートや解いた問題数は関係ありません。
+                        <br />
+                        競技プログラミングを初めたばかりの人もそうでない人も平等に作問に取り組むことができます。
+                    </p>
+                </Layout>
+                <Layout>
+                    <h4>シェアしよう！</h4>
+                    <p>
+                        作った問題はTwitterで共有してみましょう！
+                        <br />
+                        フォロワーのみなさんがあなたの問題を楽しんでくれるはずです！
+                    </p>
+                </Layout>
+                <Layout>
+                    <h3>お問い合わせ</h3>
                     <div>
-                        <IconWithText icon={<SmileyIcon />}>
-                            <a
-                                href={TWITTER_LINK}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                @makutamoto
-                            </a>
-                        </IconWithText>{' '}
+                        <a href={TWITTER_LINK} target="_blank" rel="noreferrer">
+                            @makutamoto
+                        </a>{' '}
                         <IconWithText icon={<MarkGithubIcon />}>
                             <a
                                 href={GITHUB_LINK}
@@ -44,11 +82,8 @@ export const Index: React.FC = () => {
                             </a>
                         </IconWithText>
                     </div>
-                </div>
-            </Top>
-            <Layout>
-                <h2>{t`timeline`}</h2>
-            </Layout>
+                </Layout>
+            </div>
         </>
     )
 }
