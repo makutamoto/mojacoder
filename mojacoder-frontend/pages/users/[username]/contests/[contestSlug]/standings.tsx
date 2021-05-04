f8a16d531ae3dc5e6e789a2bf1744e291271f866
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2021-05-04 22:17:26 +0900

    fixed permission denied on standings

--- mojacoder-frontend/pages/users/[username]/contests/[contestSlug]/standings.tsx
+++ mojacoder-frontend/pages/users/[username]/contests/[contestSlug]/standings.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
 import gql from 'graphql-tag'
-import { Alert, Spinner, Table } from 'react-bootstrap'
+import { Spinner, Table } from 'react-bootstrap'
 
 import { useI18n } from '../../../../../lib/i18n'
 import Auth from '../../../../../lib/auth'
@@ -17,7 +17,6 @@ import ContestTop from '../../../../../containers/ContestTop'
 const Status = {
     Loading: 'Loading',
     Done: 'Done',
-    AccessDenied: 'AccessDenied',
 }
 type Status = typeof Status[keyof typeof Status]
 
@@ -27,6 +26,37 @@ interface Props {
 
 const GetContestStandings = gql`
     query GetContestStandings($username: String!, $contestSlug: String!) {
+        user(username: $username) {
+            contest(slug: $contestSlug) {
+                id
+                standings {
+                    rank
+                    user {
+                        detail {
+                            userID
+                            icon
+                            screenName
+                        }
+                    }
+                    score
+                    penalty
+                    secondsFromStart
+                    submissions {
+                        penalty
+                        score
+                        secondsFromStart
+                    }
+                }
+            }
+        }
+    }
+`
+
+const GetContestStandingsWithDetail = gql`
+    query GetContestStandingsWithDetail(
+        $username: String!
+        $contestSlug: String!
+    ) {
         user(username: $username) {
             contest(slug: $contestSlug) {
                 id
@@ -69,17 +99,17 @@ const ContestStandings: React.FC<Props> = ({ contest }) => {
     const detail = realtimeContest?.detail
     const standings = realtimeContest?.standings
     useEffect(() => {
-        if (!auth) {
-            setStatus(Status.AccessDenied)
-            return
-        }
         const loadTasks = async () => {
-            const {
-                user: { contest },
-            } = await invokeQuery(GetContestStandings, {
+            setStatus(Status.Loading)
+            const args = {
                 username,
                 contestSlug,
-            })
+            }
+            const {
+                user: { contest },
+            } = auth
+                ? await invokeQuery(GetContestStandingsWithDetail, args)
+                : await invokeQueryWithApiKey(GetContestStandings, args)
             setRealtimeContest(contest)
             setStatus(Status.Done)
         }
@@ -94,9 +124,7 @@ const ContestStandings: React.FC<Props> = ({ contest }) => {
                 detail={detail}
             />
             <Layout>
-                {status === Status.AccessDenied ? (
-                    <Alert variant="danger">{t`accessDenied`}</Alert>
-                ) : status === Status.Done ? (
+                {status === Status.Done ? (
                     <Table responsive bordered striped hover>
                         <thead>
                             <tr>
