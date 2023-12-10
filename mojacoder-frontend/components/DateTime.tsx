53936c4c46f466e15fdbb87e2ccbe76ecd83dda3
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2023-12-10 16:57:02 +0900

    Next jsをアップデートしたことによる制約に対応

--- mojacoder-frontend/components/DateTime.tsx
+++ mojacoder-frontend/components/DateTime.tsx
@@ -1,22 +1,25 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export interface DateTimeProps {
     children?: string | Date
 }
 
 const DateTime: React.FC<DateTimeProps> = (props) => {
-    const datetime = useMemo(() => {
-        return new Date(props.children || null)
+    const [datetime, setDatetime] = useState<Date | null>(null)
+    const [datetimeStr, setDatetimeStr] = useState<string | null>(null)
+    useEffect(() => {
+        const date = new Date(props.children || null)
+        setDatetime(datetime)
+        setDatetimeStr(
+            `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(
+                -2
+            )}-${('0' + date.getDate()).slice(-2)} ${(
+                '0' + date.getHours()
+            ).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${(
+                '0' + date.getSeconds()
+            ).slice(-2)}`
+        )
     }, [props.children])
-    return (
-        <time dateTime={datetime.toISOString()}>{`${datetime.getFullYear()}-${(
-            '0' +
-            (datetime.getMonth() + 1)
-        ).slice(-2)}-${('0' + datetime.getDate()).slice(-2)} ${(
-            '0' + datetime.getHours()
-        ).slice(-2)}:${('0' + datetime.getMinutes()).slice(-2)}:${(
-            '0' + datetime.getSeconds()
-        ).slice(-2)}`}</time>
-    )
+    return <time dateTime={datetime?.toISOString()}>{datetimeStr}</time>
 }
 export default DateTime
