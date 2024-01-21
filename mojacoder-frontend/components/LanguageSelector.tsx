30861c47f227562fa774f352836dcb18d51e2dd1
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-01-21 21:54:44 +0900

    [Update] judge image Dockerfileの言語を更新しました
    それに伴い、フロントエンドの言語表示も対応させました。
    
    ライブラリなどはAtCoderを参考にしています
    
    **run.go**
    実行時に環境変数`PATH`を設定するようにしました。これが無いことが原因で一部言語が動かなかったためです。

--- mojacoder-frontend/components/LanguageSelector.tsx
+++ mojacoder-frontend/components/LanguageSelector.tsx
@@ -5,21 +5,21 @@ import Selector, { SelectorProps, SelectorOptionsType } from './Selector'
 export type LanguageSelectorProps = Omit<SelectorProps, 'options'>
 
 const OPTIONS: SelectorOptionsType = [
-    { value: 'go-1.14', label: 'Go (1.14)' },
-    { value: 'python3.8', label: 'Python3 (CPython 3.8)' },
-    { value: 'gcc-9.3.0', label: 'C (GCC 9.3.0)' },
-    { value: 'g++-9.3.0', label: 'C++ (GCC 9.3.0)' },
-    { value: 'csharp-mono-csc-3.6.0', label: 'C# (Mono-csc 3.6.0)' },
-    { value: 'csharp-mono-mcs-6.12.0.107', label: 'C# (Mono-mcs 6.12.0.107)' },
+    { value: 'go-1.21', label: 'Go (1.21)' },
+    { value: 'python3.11', label: 'Python3 (CPython 3.11)' },
+    { value: 'gcc-12.3', label: 'C (GCC 12.3)' },
+    { value: 'g++-12.3', label: 'C++ (GCC 12.3)' },
+    { value: 'csharp-mono-csc-3.9.0', label: 'C# (Mono-csc 3.9.0)' },
+    { value: 'csharp-mono-mcs-6.12.0', label: 'C# (Mono-mcs 6.12.0)' },
     { value: 'bf-20041219', label: 'Brainfuck (bf 20041219)' },
     { value: 'cat', label: 'Text (cat)' },
-    { value: 'rust-1.43.0', label: 'Rust (rustc 1.43.0)' },
-    { value: 'pypy3-7.3.1', label: 'Python3 (pypy3 7.3.1)' },
-    { value: 'ruby-2.7', label: 'Ruby (CRuby 2.7)' },
-    { value: 'java-11', label: 'Java 11 (Open JDK 11)' },
-    { value: 'j-902', label: 'J (j902)' },
-    { value: 'kotlin-1.4', label: 'Kotlin (1.4)' },
-    { value: 'commonlisp-2.0', label: 'Common Lisp (2.0)' },
+    { value: 'rust-1.74.0', label: 'Rust (rustc 1.74.0)' },
+    { value: 'pypy3-7.3.13', label: 'Python3 (pypy3 7.3.13)' },
+    { value: 'ruby-3.2.2', label: 'Ruby (CRuby 3.2.2)' },
+    { value: 'java-21', label: 'Java 21 (Open JDK 21)' },
+    { value: 'kotlin-1.9.21', label: 'Kotlin (1.9)' },
+    { value: 'commonlisp-2.1.11', label: 'Common Lisp (2.1)' },
+    { value: 'nim-1.6.16', label: 'Nim (1.6.16)' },
 ]
 
 const LanguageSelector: React.FC<LanguageSelectorProps> = (props) => {
